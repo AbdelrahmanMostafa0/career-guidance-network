c234ebd3a22be305160487ed7ab8a4d32d4090fd
MRZaki2000 <57083147+MRZaki2000@users.noreply.github.com>
2023-11-08 23:26:45 +0200

    add

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -15,7 +15,7 @@ export default function Home() {
   return (
     <main className={styles.mainGrid}>
       <section className={`${styles.section} ${styles.heroSection}`}>
-        <h1>CGN is Your way to success</h1>
+        <h1>CGN is Your <br/> way to success</h1>
         <img src="cgn.png" />
         <p>Empower your career journey with Carrer Guidance Network. Connect, collaborate, and thrive in our vibrant professional community. Your next opportunity awaits here. Join us today!</p>
         <Link href="#">Get Started</Link>
@@ -25,12 +25,12 @@ export default function Home() {
         {/* <h2>How it Works</h2> */}
         <div className={styles.cardsContainer}>
           <div className={styles.card}>
-            <h3>Get Ready For a Career</h3>
+            <h3>Get a Career</h3>
             <img src="build.png" />
             <p>Discover your dream career with us as We connect talented professionals with top opportunities, making job hunting simple and rewarding. Your future starts here</p>
           </div>
           <div className={styles.card}>
-            <h3>Know The Latest Skills</h3>
+            <h3>Stay ahead</h3>
             <img src="learn.png"/>
             <p>Stay ahead with us and Access a curated Roadmap and expert insights to master the latest skills. Empower your professional growth and embrace a future of endless possibilities</p>
           </div>
@@ -42,8 +42,23 @@ export default function Home() {
         </div>
       </section>
 
-      <section>
-        {/* <h2>Visit our Roadmap</h2> */}
+      <section className={`${styles.section} ${styles.roadmapSection}`}>
+        <h2>Visit our Roadmaps</h2>
+        <div className={styles.careersContainer}>
+          <div className={styles.career}>
+            <h3>Front-end career path</h3>
+            <p>Explore the art of user interaction with Front-End Development. Learn to create seamless user experiences, design intuitive interfaces, and bring websites to life</p>
+          </div>
+          <div className={styles.career}>
+            <h3>Back-end career path</h3>
+            <p>Delve into the heart of web technology with Back-End Development.Discover the world of server-side programming like databases and server management</p>
+          </div>
+          <div className={styles.career}>
+            <h3>Software engineer career path</h3>
+            <p>Embark on a transformative journey in Software Engineering. Master the art and science of developing high-quality software solutions</p>
+          </div>
+        </div>
+        <Link href="#">Discover Now</Link>
       </section>
     </main>
   );
