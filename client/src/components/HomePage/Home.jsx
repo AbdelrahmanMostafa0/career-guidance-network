ce3f3a181d6304c4f449b8c7c87b19b97507c81c
Abdelrahman Mostafa <abdelrahmanmostafa.developer@gmail.com>
2023-11-22 22:55:56 +0200

    add back end

--- client/src/components/HomePage/Home.jsx
+++ client/src/components/HomePage/Home.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 // import { motion } from "framer-motion";
 import Link from "next/link";
-import HeroSection from "./sections/HeroSection";
-import KeyFeatures from "./sections/KeyFeatures";
-import HowItWorks from "./sections/HowItWorks";
+import HeroSection from "./NotSigned/sections/HeroSection";
+import KeyFeatures from "./NotSigned/sections/KeyFeatures";
+import HowItWorks from "./NotSigned/sections/HowItWorks";
 const Home = () => {
   return (
     <main className="w-[95%] sm:w-[90%] mx-auto my-5">
@@ -11,12 +11,13 @@ const Home = () => {
       <HeroSection />
       <KeyFeatures />
       <HowItWorks />
-      <section className="flex  p-7 md:px-14 gap-10">
-        {/* <img
-          src="/handshake.jpg"
-          className="w-1/2 drop-shadow-lg rounded-lg border"
-          alt=""
-        /> */}
+      <section className="flex  p-7 md:px-14 gap-10 items-center justify-center">
+        <Link
+          href={"/signup"}
+          className="border w-96 text-center p-3 bg-lighterGreen rounded-full text-white font-bold hover:bg-lightGreen duration-150"
+        >
+          Join US Now
+        </Link>
       </section>
     </main>
   );
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -2,17 +2,28 @@ import Link from "next/link";
 import { useState } from "react";
 import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
 import MobileMenu from "./MobileMenu";
+import { Notifications } from "@mui/icons-material";
 
 const Navbar = () => {
   const [showMenu, setShowMenu] = useState(false);
-  const [signedIn, setSignedIn] = useState(true);
-
+  const [signedIn, setSignedIn] = useState(false);
+  const [showNotification, setShowNotification] = useState(false);
   const toggleMobMenu = () => {
     setShowMenu((prevState) => !prevState);
   };
 
+  const toggleState = (setState) => {
+    setState((prevState) => !prevState);
+  };
+
   return (
-    <nav className="px-5  py-5  border-b ">
+    <nav className="px-5  py-5  border-b relative">
+      <button
+        className="absolute top-0 left-0 text-xs opacity-10"
+        onClick={() => toggleState(setSignedIn)}
+      >
+        signin
+      </button>
       {/* logo and links */}
       <div className="md:w-[90%] flex items-center justify-between mx-auto">
         <div className="flex items-center gap-10">
@@ -41,19 +52,56 @@ const Navbar = () => {
         </div>
         {/* Sign up sign in */}
         <div className="flex items-center gap-3">
-          <Link
-            href={"/signin"}
-            className="border-2 border-lighterGreen px-6 py-1 md:text-lg font-bold text-lighterGreen duration-150 hover:bg-lighterGreen hover:text-white rounded-full"
+          {!signedIn ? (
+            <>
+              <Link
+                href={"/signin"}
+                className="border-2 border-lighterGreen px-6 py-1 md:text-lg font-bold text-lighterGreen duration-150 hover:bg-lighterGreen hover:text-white rounded-full"
+              >
+                Sign in
+              </Link>
+              <Link
+                href={"/signup"}
+                className="border-2 md:block hidden border-lighterGreen px-6 py-1 text-lg font-bold hover:bg-transparent duration-150 hover:text-lighterGreen bg-lighterGreen text-white rounded-full"
+              >
+                Sign Up
+              </Link>
+            </>
+          ) : (
+            <>
+              <div className="relative">
+                <button onClick={() => toggleState(setShowNotification)}>
+                  <Notifications className="text-lightGreen text-5xl" />
+                </button>
+                {showNotification && (
+                  <div className="bg-white drop-shadow-lg rounded-lg w-60 border absolute -right-5 my-1 p-3 z-50">
+                    <h3 className="font-bold text-xl">Notifications</h3>
+                    <p className="p-1 rounded-lg bg-slate-200 my-1 line-clamp-1 ">
+                      Notifications 1
+                    </p>
+                    <p className="p-1 rounded-lg bg-slate-200 my-1 line-clamp-1 ">
+                      Notifications 2
+                    </p>
+                    <p className="p-1 rounded-lg bg-slate-200 my-1 line-clamp-1 ">
+                      Notifications 3
+                    </p>
+                  </div>
+                )}
+              </div>{" "}
+              <Link href={"/profile"}>
+                <img
+                  src="/user.jpg"
+                  alt=""
+                  className="w-14 h-14 object-cover rounded-full border-4 border-lightGreen"
+                />
+              </Link>
+            </>
+          )}
+
+          <button
+            className="block md:hidden"
+            onClick={() => toggleState(setShowMenu)}
           >
-            Sign in
-          </Link>
-          <Link
-            href={"/signup"}
-            className="border-2 md:block hidden border-lighterGreen px-6 py-1 text-lg font-bold hover:bg-transparent duration-150 hover:text-lighterGreen bg-lighterGreen text-white rounded-full"
-          >
-            Sign Up
-          </Link>
-          <button className="block md:hidden" onClick={toggleMobMenu}>
             <MenuOutlinedIcon />
           </button>
         </div>
