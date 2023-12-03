316c8e7f75206f5079a3d77bd02989774bb9a6e8
MRZaki2000 <57083147+MRZaki2000@users.noreply.github.com>
2023-12-03 05:20:00 +0200

    adding view job

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,9 +1,10 @@
 import Home from "@/components/HomePage/Home";
+import ViewJob from "@/components/veiwjob/ViewJob";
 
 export default function index() {
   return (
     <div className="min-h-[90dvh]">
-      <Home />
+      <ViewJob />
     </div>
   );
 }
