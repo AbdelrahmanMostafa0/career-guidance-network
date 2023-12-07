74cdd7f3a8ded0db096be19debbb1112ed3ef2cd
MRZaki2000 <57083147+MRZaki2000@users.noreply.github.com>
2023-12-07 02:42:43 +0200

    adding newsfeed

---
+++ client/src/components/newsfeed/Newsfeed.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Link from "next/link"
+import styles from "../../styles/newsfeed.module.css"
+
+export default function Newsfeed() {
+  return (
+    <main className={styles.mainContainer}>
+        <div className={styles.newsfeedContainer}>
+            <div className={`${styles.feed} ${styles.flow}`}>
+                <div className={styles.companyInfo}>
+                    <Link href="#">
+                        <div className={styles.InnerCompanyInfo}>
+                            <img className={styles.companyLogo} src='googlelogo.png' alt='company logo'/>
+                            <p className={styles.companyName}>Google</p>
+                        </div>
+                    </Link>
+                    <p className={styles.time}>2m Ago</p>
+                </div>
+                <h3 className={styles.jobTitle}>Front end</h3>
+                <p className={styles.jobInfo}>Full Time - <span>on site</span></p>
+                <p className={styles.jobDescription}>Lorem ipsum dolor sit amet consectetur. Nibh ac facilisis senectus tristique. Pretium nunc quam facilisis nulla. Proin pretium a mattis parturient nunc. Eget libero morbi malesuada lectus quisque amet molestie </p>
+                <div className={styles.btnsContainer}>
+                    <button className={`${styles.btn} ${styles.detailsBtn}`}>Details</button>
+                    <button className={`${styles.btn} ${styles.applyBtn}`}>Apply</button>
+                </div>
+            </div>
+        </div>
+        <div className={styles.asideContainer}>
+
+        </div>
+    </main>
+  )
+}
+
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,10 +1,11 @@
 import Home from "@/components/HomePage/Home";
 import ViewJob from "@/components/veiwjob/ViewJob";
+import Newsfeed from "@/components/newsfeed/Newsfeed";
 
 export default function index() {
   return (
     <div className="min-h-[90dvh]">
-      <ViewJob />
+      <Newsfeed />
     </div>
   );
 }
